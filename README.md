333663e88930f2f74f33eab8f0ac8730830fa301
Aubin9 <aubinsiaha9@gmail.com>
2024-03-19 11:27:16 +0100

    Basics

--- README.md
+++ README.md
@@ -5,3 +5,10 @@
 -tools: VS code(text editor); node(Run time environment to execute JS code); Live server extention(on VS code);
 -Hello world in JS
 -Run in node: open the terminal > node index.js
+
+## Basics
+
+-Variables and constant: let, var, const
+-Primitive type / value types: String, Number, Boolean, undefined, null
+-Dynamic typing
+-Reference Types: Object, Array, Function
--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 console.log("Hello world !!");
+let name = "Aubin";
+let age = 19;
+let isApproved = false;
+let firstName = undefined;
+let selectColor = null;
+console.log(typeof age);
+
+// OBJECTS
+let person = {
+  name: "Aubin",
+  age: 19,
+};
+console.log(person.name);
+
+// ARRAYS
+let selectedColors = ["green", "red", "tomato", "gold", "cian", "blue"];
+console.log(selectedColors);
+console.log(selectedColors[2]);
+console.log(selectedColors.length);
+
+//FUNCTIONS
+function greet(name) {
+  console.log("Hello " + name);
+}
+greet(person.name);
